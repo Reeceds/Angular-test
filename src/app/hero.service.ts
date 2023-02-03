eb26b89d6b2d891c8a85981ebe25054bd4475746
Reeceds <32424143+Reeceds@users.noreply.github.com>
2023-02-03 11:48:13 -0500

    started http section

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { Hero } from './data/hero';
 import { HEROES } from './data/mock-heroes';
@@ -9,22 +11,44 @@ import { MessageService } from './message.service';
   providedIn: 'root',
 })
 export class HeroService {
-    constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) {}
 
-    // Observable is used for async data handling
-    // 'of' is used to return a single value from the array and store it in a var
-    getHeroes(): Observable<Hero[]> {
-        const heroes = of(HEROES);
-        this.messageService.add('HeroService: fetched heroes');
-        return heroes;
-    };
+  private heroesUrl = 'api/heroes'; // URL to web api
 
-    getHero(id: number): Observable<Hero> {
-        // For now, assume that a hero with the specified `id` always exists.
-        // Error handling will be added in the next step of the tutorial.
-        const hero = HEROES.find(h => h.id === id)!;
-        this.messageService.add(`HeroService: fetched hero id=${id}`);
-        return of(hero);
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
     };
-    
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  // Observable is used for async data handling
+  // 'of' is used to return a single value from the array and store it in a var
+  getHeroes(): Observable<Hero[]> {
+    return this.http
+      .get<Hero[]>(this.heroesUrl)
+      .pipe(catchError(this.handleError<Hero[]>('getHeroes', [])));
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // For now, assume that a hero with the specified `id` always exists.
+    // Error handling will be added in the next step of the tutorial.
+    const hero = HEROES.find((h) => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
+  }
 }
